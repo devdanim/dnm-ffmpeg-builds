fa698fe13f47c0793e320a096ec7bb86cf95dc51
Adrien du Repaire <adrien@danim.com>
2020-11-04 15:41:17 +0100

    umd

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-export default {
-    "version": "4.3.1",
-    "license": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/FFMPEG-LICENSE.txt",
-    "source": "https://github.com/FFmpeg/FFmpeg/archive/n4.3.1.zip",
-    "builds": {
-        "win": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-win64.zip",
-        "mac": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-macos64.zip"
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['b'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('b'));
+    } else {
+        root.returnExports = factory(root.b);
     }
-}
+}(typeof self !== 'undefined' ? self : this, function (b) {
+    return {
+        "version": "4.3.1",
+        "license": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/FFMPEG-LICENSE.txt",
+        "source": "https://github.com/FFmpeg/FFmpeg/archive/n4.3.1.zip",
+        "builds": {
+            "win": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-win64.zip",
+            "mac": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-macos64.zip"
+        }
+    };
+}));
