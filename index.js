1e915fd371600c457c4300f030f79b6428633296
Adrien du Repaire <adrien@danim.com>
2020-11-04 15:22:58 +0100

    4.3.1

---
+++ index.js
@@ -0,0 +1,9 @@
+export default {
+    "version": "4.3.1",
+    "license": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/FFMPEG-LICENSE.txt",
+    "source": "https://github.com/FFmpeg/FFmpeg/archive/n4.3.1.zip",
+    "builds": {
+        "win": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-win64.zip",
+        "mac": "https://raw.githubusercontent.com/devdanim/dnm-ffmpeg-builds/main/bin/ffmpeg-macos64.zip"
+    }
+}
